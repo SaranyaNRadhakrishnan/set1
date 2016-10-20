ce7f4112c9d448affb51f45b43e524ec8f4e684e
SaranyaNRadhakrishnan <rsaranya905@gmail.com>
2016-10-20 11:34:39 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a1;
+printf("Enter the letter:");
+scanf("%c",&a1);
+if(a1=='a'||a1=='e'||a1=='i'||a1=='o'||a1=='u');
+printf("vowel);
+else
+printf("not vowel");
+return 0;
+}
