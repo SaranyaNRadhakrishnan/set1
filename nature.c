41df76fd771b1dcd83aaaf5df549f7e993d6256e
SaranyaNRadhakrishnan <rsaranya905@gmail.com>
2016-10-20 14:48:40 +0530

    Create nature.c

---
+++ nature.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,sum=0;
+printf("Enter the integer");
+scanf(%d",&n);
+i=1
+while(i<n)
+{
+sum +=i;
+i++;
+}
+printf("sum =%d",sum)
+return 0;
+}
