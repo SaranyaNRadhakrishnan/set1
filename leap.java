797e4075947c24e74ef2c7e6614372386c6539c1
SaranyaNRadhakrishnan <rsaranya905@gmail.com>
2016-10-20 14:08:01 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+public static void main(String args[])
+{
+int year=2000;
+if(year%4==0)
+{
+System.out.println("leap");
+}
+else
+{
+System.out.println("not leap");
+}
+return 0
+}
